10a8318624940fdac92f7317dd4d4d1af51835ef
hoodie Coder <mayank4kathuria@gmail.com>
2019-02-24 14:56:22 +0530

    Main Component for NexsX React Application

---
+++ newsx/src/mainComponent.js
@@ -0,0 +1,43 @@
+// The main component of the project
+
+import React from "react";
+import {getSources} from "./api";
+import {NewsBox} from "./newsBox.js";
+
+class MainComponent extends React.Component {
+  state = {
+    sources: [],
+  }
+
+  ss = (val) => {
+
+      if( val.sources.length > 0){
+        this.setState({
+          sources: val.sources,
+        });
+      }
+ }
+
+  componentDidMount(){
+    getSources(this.ss);
+  }
+
+  render(){
+    return (
+        <div className="container">
+          { this.state.sources.map(
+              (val) => <NewsBox source={val}/> ) 
+            || <p className="no-news">No News Item is Avaiable</p> }
+            <div className="footer">
+              <footer> MADE WITH LOVE,SWEAT,HARD WORK AND GOOGLE SEARCHES BY 
+                <span><a href="https://github.com/mayank4kathuria" target="_blank"> MAYANK KATHURIA</a></span> 
+              </footer>
+            </div>
+        </div>
+      );
+  }
+
+}
+
+
+export default MainComponent;
