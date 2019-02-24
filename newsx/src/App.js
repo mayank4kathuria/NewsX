787654b4a5437241b53fa7444ff875dbe96778ff
hoodie Coder <mayank4kathuria@gmail.com>
2019-02-24 19:45:30 +0530

    Removde redundant Line

--- newsx/src/App.js
+++ newsx/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {Route} from "react-router-dom";
 import MainComponent from "./mainComponent.js";
 import NewsViewer from "./newsViewer.js";
+import './App.css';
 
 
 class App extends Component {
